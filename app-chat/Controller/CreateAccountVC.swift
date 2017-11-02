acd14ad8d0c70e5bc3b4d8e51f57c3947fa70ac8
nykielmat <nykielmat@gmail.com>
2017-11-02 22:53:52 +0100

    IBOutlets and IBActions connected

--- app-chat/Controller/CreateAccountVC.swift
+++ app-chat/Controller/CreateAccountVC.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //Outlets
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
 
-        
+    @IBAction func createAccountPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
     @IBAction func closePressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)    } //back to main VC
     
