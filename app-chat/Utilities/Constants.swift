4f52a0afeac7242918f586e61367732aa34887de
nykielmat <nykielmat@gmail.com>
2017-11-03 15:10:47 +0100

    added user login

--- app-chat/Utilities/Constants.swift
+++ app-chat/Utilities/Constants.swift
@@ -12,18 +12,21 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 // URL Contants
 let BASE_URL = "https://chatitty.herokuapp.com/v1/"
-let URL_REGISTER = "\(BASE_URL)account/register/"
-//Seques
+let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
+//Seques
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
 
-
-
 // User Defaults
-
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+// Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
 
