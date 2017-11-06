a3634b06b6d5dc3fba768c9fbe963329fea75e32
nykielmat <nykielmat@gmail.com>
2017-11-06 15:13:19 +0100

    avatar cell for avatar picker vc

--- app-chat/Controller/AvatarPickerVC.swift
+++ app-chat/Controller/AvatarPickerVC.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class AvatarPickerVC: UIViewController, UICollectionViewDelegate, //UICollectionViewDataSource,
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource,
 UICollectionViewDelegateFlowLayout {
-
+    
     
     //Outlets
     @IBOutlet weak var collectionView: UICollectionView!
@@ -23,6 +23,24 @@ UICollectionViewDelegateFlowLayout {
         collectionView.dataSource = self
 
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell{
+            return cell
+        } else {
+            return AvatarCell()
+        }
+        
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
     //Actions
     @IBAction func backPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
---
+++ app-chat/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  app-chat
+//
+//  Created by mateusz on 06.11.2017.
+//  Copyright © 2017 mateusz. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView(){
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
