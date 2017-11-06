c20b6971e096270cc4e96835a106a977dba45315
nykielmat <nykielmat@gmail.com>
2017-11-06 13:27:37 +0100

    added user data service

---
+++ app-chat/Services/UserDataService.swift
@@ -0,0 +1,26 @@
+//
+//  UserDataService.swift
+//  app-chat
+//
+//  Created by mateusz on 06.11.2017.
+//  Copyright © 2017 mateusz. All rights reserved.
+//
+
+import Foundation
+
+class UserDataService {
+    
+    static let instance = UserDataService()
+    
+    public private(set) var id = ""
+    public private(set) var avatarColor = ""
+    public private(set) var avatarName = ""
+    public private(set) var email = ""
+    public private(set) var name = ""
+    
+    func setUserData() {
+        
+    }
+
+    
+}
