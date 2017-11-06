10936237d2a6e889996fead1f55e501ec6d50e0f
nykielmat <nykielmat@gmail.com>
2017-11-06 15:42:25 +0100

    responsive avatar list se<->8+

--- app-chat/Controller/AvatarPickerVC.swift
+++ app-chat/Controller/AvatarPickerVC.swift
@@ -49,7 +49,25 @@ UICollectionViewDelegateFlowLayout {
         dismiss(animated: true, completion: nil)
     }
     @IBAction func segmentControlChanged(_ sender: Any) {
+        
+        if segmentControl.selectedSegmentIndex == 0 {
+            avatarType = .dark
+        } else {
+            avatarType = .light
+        }
+        collectionView.reloadData()
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var numOfColumns : CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+            numOfColumns = 4
+        }
+        
+        let spaceBetweenCells : CGFloat = 10
+        let padding : CGFloat = 40
+        let cellDimension = ((collectionView.bounds.width - padding) - (numOfColumns - 1) * spaceBetweenCells) / numOfColumns
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
 
 }
