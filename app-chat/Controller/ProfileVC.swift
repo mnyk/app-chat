4dab7c226d2b7e541a665cfd5ab1d1bb5645fc77
nykielmat <nykielmat@gmail.com>
2017-11-29 20:33:08 +0100

    ibo mounted

--- app-chat/Controller/ProfileVC.swift
+++ app-chat/Controller/ProfileVC.swift
@@ -10,26 +10,30 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
+    
+    
+    //outlets
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var userEmail: UILabel!
+    
+    @IBOutlet weak var bgView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func closeModalPressed(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutPressed(_ sender: Any) {
     }
-    */
-
+    
 }
